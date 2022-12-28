000639a704166b3034ad0efa14ad36844ba63b36
David Abreu <davidabreu.dev@gmail.com>
2022-12-28 10:36:55 -0300

    log messages configured

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -4,17 +4,19 @@ import com.ead.authuser.dto.UserDto;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+
+@Log4j2
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class AuthenticationController {
+
 
     private final UserService userService;
 
@@ -27,6 +29,8 @@ public class AuthenticationController {
             @JsonView(UserDto.UserView.RegistrationPost.class)
             @RequestBody @Validated(UserDto.UserView.RegistrationPost.class) UserDto userModel)
     {
+        log.debug("POST /auth/signup createUser userDTO received : {}", userModel.toString());
+
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(userModel));
     }
 
