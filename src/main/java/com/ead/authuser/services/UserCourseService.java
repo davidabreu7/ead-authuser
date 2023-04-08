0162ae54cb96904ccede97e4ad36ecfea214643a
david <davidabreu.dev@gmail.com>
2023-04-08 17:17:41 -0300

    configured config-server

--- src/main/java/com/ead/authuser/services/UserCourseService.java
+++ src/main/java/com/ead/authuser/services/UserCourseService.java
@@ -2,6 +2,7 @@ package com.ead.authuser.services;
 
 import com.ead.authuser.client.UserCourseClient;
 import com.ead.authuser.dto.CourseRecord;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -12,14 +13,19 @@ import org.springframework.stereotype.Service;
 public class UserCourseService {
    private final UserCourseClient userCourseClient;
 
-    public UserCourseService(UserCourseClient userCourseClient) {
+   public UserCourseService(UserCourseClient userCourseClient) {
         this.userCourseClient = userCourseClient;
     }
 
 
+    @CircuitBreaker(name = "course-service", fallbackMethod = "fallback")
     public Page<CourseRecord> getAllCoursesByStudent(String id) {
-
         return userCourseClient.getAllCoursesByStudent(id);
+    }
+
+    public Page<CourseRecord> fallback(String id, Exception e) {
+        log.error("(circuit-breaker) Error: " + e.getMessage());
+        return Page.empty();
     }
 
 }
